4595127feed3fa9ad847a1d971286a8c6368c2c0
MotteyAmos <akamottey@st.ug.edu.gh>
2024-06-14 16:02:50 +0000

    inheritance and method overriding push

---
+++ InheritanceAndMethodOverriding/InheritanceAndMethodOverriding/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace InheritanceAndMethodOverriding
+{
+    //base class
+    public class Animal
+    {
+        public virtual void makeSound()
+        {
+            Console.WriteLine("Some generic Sound");
+        }
+    }
+
+    public class Dog: Animal
+    {
+        public override void makeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+
+    }
+
+    public class Cat: Animal
+    {
+        public override void makeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            animal.makeSound();
+            dog.makeSound();
+            cat.makeSound();
+        }
+    }
+}
